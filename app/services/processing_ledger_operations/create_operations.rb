3e506fb59a8b2b37043f5c2540b66bb73d96f5e0
Ramon Tayag <ramon.tayag@gmail.com>
2017-03-07 11:47:25 +0800

    Add note for future work

--- app/services/processing_ledger_operations/create_operations.rb
+++ app/services/processing_ledger_operations/create_operations.rb
@@ -5,6 +5,8 @@ module ProcessingLedgerOperations
     expects :client, :ledger_sequence
 
     executed do |c|
+      # TODO be able to manage instances where there are more ops than what fit
+      # in the page
       c.client.records.each do |remote_operation|
         ProcessOperation.execute({
           remote_operation: remote_operation,
