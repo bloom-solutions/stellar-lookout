6d7db35958832683ca193c79ccadfac7f40bc6f1
Ramon Tayag <ramon.tayag@gmail.com>
2017-03-07 13:23:52 +0800

    Enqueue WardAfterCreateJob after Api::V1::WardResource is created

--- app/resources/api/v1/ward_resource.rb
+++ app/resources/api/v1/ward_resource.rb
@@ -2,7 +2,15 @@ module Api
   module V1
     class WardResource < JSONAPI::Resource
 
+      after_create :enqueue_after_create_job
+
       attributes *%i[address callback_url secret]
+
+      private
+
+      def enqueue_after_create_job
+        WardAfterCreateJob.perform_later(@model)
+      end
 
     end
   end
--- spec/resources/api/v1/ward_resource_spec.rb
+++ spec/resources/api/v1/ward_resource_spec.rb
@@ -11,6 +11,21 @@ module Api
       it { is_expected.to have_attribute(:callback_url) }
       it { is_expected.to have_attribute(:secret) }
 
+      describe "after_create" do
+        let(:ward) { create(:ward) }
+        let(:resource) { described_class.new(ward, {}) }
+
+        it "enqueues WardAfterCreateJob" do
+          # Don't know how to save a resource manually. Ugly hack it by
+          # 1) ensuring the callback is there
+          # 2) call the callback (`send`) and check that the Job was queued
+          expect(resource._create_callbacks.map(&:filter)).
+            to include(:enqueue_after_create_job)
+          resource.send(:enqueue_after_create_job)
+          expect(WardAfterCreateJob).to have_been_enqueued.with(ward)
+        end
+      end
+
     end
   end
 end
